5daf4bf402ad1b9354b9949035c699278dc2280a
Arbendu Pratihar <arbendupratihar@gmail.com>
2025-05-02 08:14:12 +0530

    Initial commit

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,130 @@
+import React from "react";
+import Slider from "react-slick";
+
+const TestimonialsData = [
+  {
+    id: 1,
+    name: "Victor",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas autem id, numquam nam blanditiis nemo fuga! Quasi perferendis fugiat cupiditate! Beatae eos iusto aliquam tempore",
+    img: "https://picsum.photos/101/101",
+  },
+  {
+    id: 2,
+    name: "Virat Kohli",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas autem id, numquam nam blanditiis nemo fuga! Quasi perferendis fugiat cupiditate! Beatae eos iusto aliquam tempore",
+    img: "https://picsum.photos/102/102",
+  },
+  {
+    id: 3,
+    name: "Arbendu",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas autem id, numquam nam blanditiis nemo fuga! Quasi perferendis fugiat cupiditate! Beatae eos iusto aliquam tempore",
+    img: "https://picsum.photos/103/103",
+  },
+  {
+    id: 4,
+    name: "Messi",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas autem id, numquam nam blanditiis nemo fuga! Quasi perferendis fugiat cupiditate! Beatae eos iusto aliquam tempore",
+    img: "https://picsum.photos/103/103",
+  },
+  {
+    id: 5,
+    name: "Vickey",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas autem id, numquam nam blanditiis nemo fuga! Quasi perferendis fugiat cupiditate! Beatae eos iusto aliquam tempore",
+    img: "https://picsum.photos/103/103",
+  },
+];
+const Testimonials = () => {
+  var settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1, // show 3 cards by default
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    pauseOnHover: true,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 10000,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlider: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slideToShow: 1,
+          SlideToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="py-10 mb-10">
+      <div className="container">
+        <div className="text-center mb-10 max-w-[600px] mx-auto">
+          <p data-aos="fade-up" className="text-sm text-primary">
+            What our customers are saying
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Testimonials
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+            placeat nobis sunt veritatis dolor. Nihil magni explicabo doloremque
+            qui, sint aliquid.
+          </p>
+        </div>
+        <div data-aos="zoom-in">
+          <Slider {...settings}>
+            {TestimonialsData.map((data) => {
+              return (
+                <div className="my-6">
+                  <div
+                    key={data.id}
+                    className="flex flex-col gap-4 shadow-lg py-8
+                            px-6 mx-4 rounded-xl dark:bg-slate-600 bg-primary/10 relative"
+                  >
+                    <div className="mb-4">
+                      <img
+                        src={data.img}
+                        alt=""
+                        className="rounded-full w-20 h-20"
+                      />
+                    </div>
+                    <div className="flex flex-col items-center gap-4">
+                      <div className="space-y-3">
+                        <p className="text-xs dark:text-slate-300 text-gray-500">
+                          {data.text}
+                        </p>
+                        <h1 className="text-xl font-bold dark:text-slate-300 text-black/80 dark:text-light">
+                          {data.name}
+                        </h1>
+                      </div>
+                    </div>
+                    <p className="text-black/20 text-9xl font-serif absolute top-0 right-0"></p>
+                  </div>
+                </div>
+              );
+            })}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
